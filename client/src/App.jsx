d1e60c9b1303c00ceb5c0a62c0ebb082def7af30
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-07-01 14:02:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React,{lazy} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from "./pages/Home"
+const Home=lazy(()=>import('./pages/Home'))
+
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<h1>About</h1>}/>
+        <Route path="*" element={<h1>Notfound</h1>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
