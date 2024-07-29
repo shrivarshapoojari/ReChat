c4e15fe5d70a24adca41e81e1cdbbb7fe2801826
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-07-29 23:31:08 +0530

    Login design completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react'
-import { Button, Container, Paper, TextField, Typography } from "@mui/material"
+import { Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography } from "@mui/material"
+import {CameraAlt as CameraAltIcon} from '@mui/icons-material'
+import { VisuallyHiddenInput } from '../components/styles/StyledComponents'
+import {useFileHandler, useInputValidation,useStrongPassword} from '6pp'
 
+import {usernameValidator}  from '../utils/validators'
 const Login = () => {
 
 
     const [isLogin, setIsLogin] = useState(true)
     const toggleLogin=()=>setIsLogin((prev)=>!prev)
+    const name=useInputValidation("")
+    const bio=useInputValidation("")
+    const username=useInputValidation("",usernameValidator)
+    const password=useInputValidation("")
+    const avatar=useFileHandler("single")
+
+    const handleLogin=(e)=>{
+        e.preventDefault
+    }
+    const handleSignup=(e)=>{
+     e.preventDefault
+    }
     return (
+
     <Container component={"main"} 
     maxWidth="xs"
     sx={{height:"100vh",
@@ -38,13 +55,17 @@ const Login = () => {
 
 
                 <Typography variant='h5'>Login</Typography>
-                <form>
+                <form
+                onSubmit={handleLogin}
+                >
                     <TextField
                         required
                         fullWidth
-                        label="Username"
+                        label="username"
                         margin='normal'
                         variant='outlined'
+                        value={username.value}
+                        onChange={username.changeHandler}
                     />
                     <TextField
                         required
@@ -53,6 +74,8 @@ const Login = () => {
                         label="Password"
                         margin='normal'
                         variant='outlined'
+                        value={password.value}
+                        onChange={password.changeHandler}
                     />
 
 
@@ -92,20 +115,89 @@ const Login = () => {
 
 
                     <Typography variant='h5'>Register</Typography>
-                    <form>
+                    <form
+                      style={{
+                        width:"100%",
+                        marginTop:"1rem"
+                      }}
+                      onSubmit={handleSignup}
+                      > 
+
+                       
+                        <Stack 
+                        position={"relative"} 
+                        width={"10rem"} 
+                        margin={"auto"}
+                        >
+
+                                 <Avatar
+                                 sx={{
+                                    width:"10rem",
+                                    height:"10rem",
+                                    objectFit:"contain"
+                                 }}
+                                 src={avatar.preview}/>
+
+
+                                 <IconButton
+                                 sx={{
+                                    position:"absolute",
+                                    bottom:"0",
+                                    right:"0",
+                                    color:"white",
+                                    bgcolor:"rgba(0,0,0,0.5)",
+                                    ":hover":{
+                                        bgcolor:"rgba(0,0,0,0.7)"
+                                    }
+                                 }}
+                                 component="label"
+                                 >
+                                    <>
+                                    <CameraAltIcon/>
+                                    <VisuallyHiddenInput type='file' onChange={avatar.changeHandler}/>
+                                    </>
+                                 </IconButton>
+                        </Stack>
+                        {avatar.error && (
+                  <Typography 
+                  m={"1rem auto"}
+                  width={"fit-content"}
+                  display={block}
+                  color="error"
+                   variant="caption">
+                    {avatar.error}
+                  </Typography>)}
                         <TextField
                             required
                             fullWidth
                             label="Name"
                             margin='normal'
                             variant='outlined'
+                            value={name.value}
+                            onChange={name.changeHandler}
                         />
+                        <TextField
+                            required
+                            fullWidth
+                            label="UserName"
+                            margin='normal'
+                            variant='outlined'
+                            value={username.value}
+                            onChange={username.changeHandler}
+                        />
+                       {username.error && (
+                  <Typography color="error" variant="caption">
+                    {username.error}
+                  </Typography>
+                )}
                            <TextField
                             required
                             fullWidth
                             label="Bio"
                             margin='normal'
                             variant='outlined'
+                            value={bio.value}
+                            onChange={bio.changeHandler}
                         />
                         <TextField
                             required
@@ -114,8 +206,11 @@ const Login = () => {
                             label="Password"
                             margin='normal'
                             variant='outlined'
+                            value={password.value}
+                            onChange={password.changeHandler}
+
                         />
-    
+                                
     
                         <Button
                             variant='contained'
