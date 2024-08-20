c939253b74c4905b03008989d4df1c8b07e0e0d6
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-20 18:56:59 +0530

    ui updates

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,53 +1,92 @@
 import React from 'react'
-import { AppBar, Box, Icon, IconButton, Menu, Toolbar } from '@mui/material'
-import {Typography} from '@mui/material'
+import { AppBar, Box, Icon, IconButton, Menu, Toolbar, Tooltip } from '@mui/material'
+import { Typography } from '@mui/material'
 import { orange } from '../../constants/color'
-import{Menu as  MenuIcon} from '@mui/icons-material'
+import { Menu as MenuIcon, Search as SearchICon} from '@mui/icons-material'
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import GroupsIcon from '@mui/icons-material/Groups';
+import { useNavigate } from "react-router-dom";
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 const Header = () => {
-  const handleMobile=()=>{
+  const navigate = useNavigate();
+  const handleMobile = () => {
     console.log('mobile')
   }
+  const openSearch = () => {
+    console.log('search');
+  }
+  const openNewGroup = () => {
+    console.log('new group');
+  }
+  
+const navigateToGroup = () => {
+  navigate('/groups')
+}
+const logoutHandler = () => {   
+  console.log('logout') ;
+  
+}
   return (
     <>
-      <Box 
-          sx={{
-            flexGrow:1
-          }}
-          height={"4rem"}
+      <Box
+        sx={{
+          flexGrow: 1
+        }}
+        height={"4rem"}
       >
-     <AppBar position='static'
-     sx={{
-      bgcolor:orange
-     }}
-     >
-      <Toolbar>
+        <AppBar position='static'
+          sx={{
+            bgcolor: orange
+          }}
+        >
+          <Toolbar>
 
-          <Typography
-          variant='h6'
-          sx={
-            {
-              display: { xs: 'none', sm: 'block' },
-            }
-          }
-          >Rechat</Typography>
-<Box
-sx={{
-  display: { xs: 'block', sm: 'none' },
-}}
->
-  <IconButton color='inherit' onClick={handleMobile}>
-     <MenuIcon/>
-  </IconButton>
+            <Typography
+              variant='h6'
+              sx={
+                {
+                  display: { xs: 'none', sm: 'block' },
+                }
+              }
+            >Rechat</Typography>
+            <Box
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+              }}
+            >
+              <IconButton color='inherit' onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
 
-  
-  </Box>     
-  
-     <Box sx={{flexGrow:1}}/>
-     <Box>
-      {/* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */}
-      </Box>  
-   </Toolbar>
-     </AppBar>
+
+            </Box>
+
+            <Box sx={{ flexGrow: 1 }} />
+            <Box>
+              <Tooltip title="Search">
+                <IconButton color='inherit' size="large" onClick={openSearch}>
+                  <SearchICon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="New Group">
+                <IconButton color='inherit' size="large" onClick={openNewGroup}>
+                  <GroupAddIcon />
+                </IconButton>
+              </Tooltip>
+            <Tooltip title="Manage Groups">
+            <IconButton color='inherit' size="large" onClick={navigateToGroup}>
+                  <GroupsIcon />
+                </IconButton>
+              </Tooltip>
+            <Tooltip title="Logout">
+            <IconButton color='inherit' size="large" onClick={logoutHandler}>
+                 <PowerSettingsNewIcon />
+                </IconButton>
+              </Tooltip>
+
+            </Box>
+          </Toolbar>
+        </AppBar>
 
       </Box>
 
