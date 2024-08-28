cd9f78d04dd716480249f7847c0204616b528f75
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-28 15:36:55 +0530

    Search Dialog

---
+++ client/src/components/shared/UserItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import memo from 'react'
+const UserItem = (user,handler,handlerIsLoading) => {
+
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default memo(UserItem)
--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,10 +1,42 @@
+import { Dialog, DialogTitle,InputAdornment,Stack,TextField,List, ListItem, ListItemText } from '@mui/material'
 import React from 'react'
-
+import { useInputValidation } from '6pp'
+import SearchIcon from '@mui/icons-material/Search';
 const Search = () => {
+  const users=[];
+  const search=useInputValidation("")
   return (
-    <div>
-      Search
-    </div>
+    <Dialog open>
+        <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+                <DialogTitle  textAlign={"center"}>Find Friends</DialogTitle>
+                <TextField   label="" value={search.value} onChange={search.changeHandler}
+                  variant="outlined"
+                  size="small"
+                  InputProps={{
+                    startAdornment:(
+                      <InputAdornment position="start">
+                        <SearchIcon/>
+                      </InputAdornment>
+                    )
+                  }}
+                
+                
+                /> 
+
+                <List>
+
+                        {
+                          users?.map((user) =>
+                            <ListItem>
+                                 <ListItemText />
+                            </ListItem>
+                        
+                        )
+                        }
+
+                </List>
+        </Stack>
+    </Dialog>
   )
 }
 
