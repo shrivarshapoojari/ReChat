b37fba3973e530d3c9f5acbe579fc60bca89d870
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-28 16:27:24 +0530

    Notification dialog started

--- client/src/components/specific/Notifications.jsx
+++ client/src/components/specific/Notifications.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Dialog,DialogTitle,Stack } from '@mui/material'
+import { sampleNotifications } from '../../constants/sampleData'
 const Notifications = () => {
   return (
-    <div>
-      Notify
-    </div>
+   <Dialog open> 
+       <Stack p={{xs:"1rem",sm:"2rem"}} maxWidth={"25rem"}>
+           <DialogTitle>Notifications</DialogTitle>
+             {
+              sampleNotifications.length >0  ?
+              
+              (<>
+              </> ):
+              
+              (<Typography textAlign={"center"}>Nothing to show here! </Typography>)
+             }
+       </Stack>
+   </Dialog>
   )
 }
 
