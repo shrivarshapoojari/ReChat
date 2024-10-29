3540d953e265bcc2aef68ae0545c025d00760145
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-10-29 16:23:30 +0530

    file menu updated

--- client/src/components/dialogs/FileMenu.jsx
+++ client/src/components/dialogs/FileMenu.jsx
@@ -1,3 +1,143 @@
+// import { ListItemText, Menu, MenuItem, MenuList, Tooltip } from "@mui/material";
+// import React, { useRef } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setIsFileMenu, setUploadingLoader } from "../../redux/reducers/misc";
+// import {
+//   AudioFile as AudioFileIcon,
+//   Image as ImageIcon,
+//   UploadFile as UploadFileIcon,
+//   VideoFile as VideoFileIcon,
+// } from "@mui/icons-material";
+// import toast from "react-hot-toast";
+//  import { useSendAttachmentsMutation } from "../../redux/reducers/api/api";
+
+// const FileMenu = ({ anchorE1, chatId }) => {
+//   const { isFileMenu } = useSelector((state) => state.misc);
+
+//   const dispatch = useDispatch();
+
+//   const imageRef = useRef(null);
+//   const audioRef = useRef(null);
+//   const videoRef = useRef(null);
+//   const fileRef = useRef(null);
+
+//   const [sendAttachments] = useSendAttachmentsMutation();
+
+//   const closeFileMenu = () => dispatch(setIsFileMenu(false));
+
+//   const selectImage = () => imageRef.current?.click();
+//   const selectAudio = () => audioRef.current?.click();
+//   const selectVideo = () => videoRef.current?.click();
+//   const selectFile = () => fileRef.current?.click();
+
+//   const fileChangeHandler = async (e, key) => {
+//     const files = Array.from(e.target.files);
+
+//     if (files.length <= 0) return;
+
+//     if (files.length > 5)
+//       return toast.error(`You can only send 5 ${key} at a time`);
+
+//     dispatch(setUploadingLoader(true));
+
+//     const toastId = toast.loading(`Sending ${key}...`);
+//     closeFileMenu();
+
+//     try {
+//       const myForm = new FormData();
+
+//       myForm.append("chatId", chatId);
+//       files.forEach((file) => myForm.append("file", file));
+
+//       const res = await sendAttachments(myForm);
+//     console.log(res)
+//       if (res.data) toast.success(`${key} sent successfully`, { id: toastId });
+//       else toast.error(`Failed to send ${key}`, { id: toastId });
+
+//       // Fetching Here
+//     } catch (error) {
+//       toast.error(error, { id: toastId });
+//     } finally {
+//       dispatch(setUploadingLoader(false));
+//     }
+//   };
+
+//   return (
+//     <Menu anchorEl={anchorE1} open={isFileMenu} onClose={closeFileMenu}>
+//       <div
+//         style={{
+//           width: "10rem",
+//         }}
+//       >
+//         <MenuList>
+//           <MenuItem onClick={selectImage}>
+//             <Tooltip title="Image">
+//               <ImageIcon />
+//             </Tooltip>
+//             <ListItemText style={{ marginLeft: "0.5rem" }}>Image</ListItemText>
+//             <input
+//               type="file"
+//               multiple
+//               accept="image/png, image/jpeg, image/gif"
+//               style={{ display: "none" }}
+//               onChange={(e) => fileChangeHandler(e, "Images")}
+//               ref={imageRef}
+//             />
+//           </MenuItem>
+
+//           <MenuItem onClick={selectAudio}>
+//             <Tooltip title="Audio">
+//               <AudioFileIcon />
+//             </Tooltip>
+//             <ListItemText style={{ marginLeft: "0.5rem" }}>Audio</ListItemText>
+//             <input
+//               type="file"
+//               multiple
+//               accept="audio/mpeg, audio/wav"
+//               style={{ display: "none" }}
+//               onChange={(e) => fileChangeHandler(e, "Audios")}
+//               ref={audioRef}
+//             />
+//           </MenuItem>
+
+//           <MenuItem onClick={selectVideo}>
+//             <Tooltip title="Video">
+//               <VideoFileIcon />
+//             </Tooltip>
+//             <ListItemText style={{ marginLeft: "0.5rem" }}>Video</ListItemText>
+//             <input
+//               type="file"
+//               multiple
+//               accept="video/mp4, video/webm, video/ogg"
+//               style={{ display: "none" }}
+//               onChange={(e) => fileChangeHandler(e, "Videos")}
+//               ref={videoRef}
+//             />
+//           </MenuItem>
+
+//           <MenuItem onClick={selectFile}>
+//             <Tooltip title="File">
+//               <UploadFileIcon />
+//             </Tooltip>
+//             <ListItemText style={{ marginLeft: "0.5rem" }}>File</ListItemText>
+//             <input
+//               type="file"
+//               multiple
+//               accept="*"
+//               style={{ display: "none" }}
+//               onChange={(e) => fileChangeHandler(e, "Files")}
+//               ref={fileRef}
+//             />
+//           </MenuItem>
+//         </MenuList>
+//       </div>
+//     </Menu>
+//   );
+// };
+
+// export default FileMenu;
+
+
 import { ListItemText, Menu, MenuItem, MenuList, Tooltip } from "@mui/material";
 import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,11 +149,10 @@ import {
   VideoFile as VideoFileIcon,
 } from "@mui/icons-material";
 import toast from "react-hot-toast";
- import { useSendAttachmentsMutation } from "../../redux/reducers/api/api";
+import { useSendAttachmentsMutation } from "../../redux/reducers/api/api";
 
 const FileMenu = ({ anchorE1, chatId }) => {
   const { isFileMenu } = useSelector((state) => state.misc);
-
   const dispatch = useDispatch();
 
   const imageRef = useRef(null);
@@ -39,22 +178,17 @@ const FileMenu = ({ anchorE1, chatId }) => {
       return toast.error(`You can only send 5 ${key} at a time`);
 
     dispatch(setUploadingLoader(true));
-
     const toastId = toast.loading(`Sending ${key}...`);
     closeFileMenu();
 
     try {
       const myForm = new FormData();
-
       myForm.append("chatId", chatId);
       files.forEach((file) => myForm.append("file", file));
 
       const res = await sendAttachments(myForm);
-    console.log(res)
       if (res.data) toast.success(`${key} sent successfully`, { id: toastId });
       else toast.error(`Failed to send ${key}`, { id: toastId });
-
-      // Fetching Here
     } catch (error) {
       toast.error(error, { id: toastId });
     } finally {
@@ -64,17 +198,13 @@ const FileMenu = ({ anchorE1, chatId }) => {
 
   return (
     <Menu anchorEl={anchorE1} open={isFileMenu} onClose={closeFileMenu}>
-      <div
-        style={{
-          width: "10rem",
-        }}
-      >
+      <div style={{ width: "12rem", padding: "0.5rem" }}>
         <MenuList>
-          <MenuItem onClick={selectImage}>
+          <MenuItem onClick={selectImage} sx={{ padding: "0.8rem", borderRadius: "8px", '&:hover': { backgroundColor: "#f0f0f0" } }}>
             <Tooltip title="Image">
-              <ImageIcon />
+              <ImageIcon color="primary" />
             </Tooltip>
-            <ListItemText style={{ marginLeft: "0.5rem" }}>Image</ListItemText>
+            <ListItemText style={{ marginLeft: "0.75rem" }}>Image</ListItemText>
             <input
               type="file"
               multiple
@@ -85,11 +215,11 @@ const FileMenu = ({ anchorE1, chatId }) => {
             />
           </MenuItem>
 
-          <MenuItem onClick={selectAudio}>
+          <MenuItem onClick={selectAudio} sx={{ padding: "0.8rem", borderRadius: "8px", '&:hover': { backgroundColor: "#f0f0f0" } }}>
             <Tooltip title="Audio">
-              <AudioFileIcon />
+              <AudioFileIcon color="secondary" />
             </Tooltip>
-            <ListItemText style={{ marginLeft: "0.5rem" }}>Audio</ListItemText>
+            <ListItemText style={{ marginLeft: "0.75rem" }}>Audio</ListItemText>
             <input
               type="file"
               multiple
@@ -100,11 +230,11 @@ const FileMenu = ({ anchorE1, chatId }) => {
             />
           </MenuItem>
 
-          <MenuItem onClick={selectVideo}>
+          <MenuItem onClick={selectVideo} sx={{ padding: "0.8rem", borderRadius: "8px", '&:hover': { backgroundColor: "#f0f0f0" } }}>
             <Tooltip title="Video">
-              <VideoFileIcon />
+              <VideoFileIcon color="action" />
             </Tooltip>
-            <ListItemText style={{ marginLeft: "0.5rem" }}>Video</ListItemText>
+            <ListItemText style={{ marginLeft: "0.75rem" }}>Video</ListItemText>
             <input
               type="file"
               multiple
@@ -115,11 +245,11 @@ const FileMenu = ({ anchorE1, chatId }) => {
             />
           </MenuItem>
 
-          <MenuItem onClick={selectFile}>
+          <MenuItem onClick={selectFile} sx={{ padding: "0.8rem", borderRadius: "8px", '&:hover': { backgroundColor: "#f0f0f0" } }}>
             <Tooltip title="File">
-              <UploadFileIcon />
+              <UploadFileIcon color="disabled" />
             </Tooltip>
-            <ListItemText style={{ marginLeft: "0.5rem" }}>File</ListItemText>
+            <ListItemText style={{ marginLeft: "0.75rem" }}>File</ListItemText>
             <input
               type="file"
               multiple
