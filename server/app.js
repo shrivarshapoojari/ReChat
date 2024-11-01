dbf2931aa8608e10ce6adb9cf70333bdc17d85ae
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-11-01 16:37:10 +0530

    production bug fixed

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ const server=createServer(app)
  
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:3000"],
+        origin: ["http://localhost:5173", "http://localhost:3000","https://rechaat.vercel.app"],
         credentials: true,
     },
     methods:["GET","POST","PUT","DELETE"]
