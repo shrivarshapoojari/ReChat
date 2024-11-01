e81176d6284f6cffbe2a62e7949df4dde7c392ef
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-11-01 19:15:12 +0530

    production bug fixed

--- server/models/otp.model.js
+++ server/models/otp.model.js
@@ -1,6 +1,6 @@
  
 import mongoose, { Schema, model, Types } from "mongoose";
-const otpSchema = new Schema({
+const schema= new Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
   expiresAt: { type: Date, required: true }
