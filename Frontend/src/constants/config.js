e6d4a4a7a94a96dbd5c8ca40b6b65f84de1f214c
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-11-05 20:53:21 +0530

    updates

--- Frontend/src/constants/config.js
+++ Frontend/src/constants/config.js
@@ -1,4 +1,4 @@
 
-// export const server="https://rechat-yhal.onrender.com"
-export const server="http://localhost:3000"
+export const server="https://rechat-yhal.onrender.com"
+// export const server="http://localhost:3000"
 
